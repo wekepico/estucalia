37569e0ebfb32774d2456cc75013d87060db6c38
ArtStyles <fejames07@gmail.com>
2025-05-16 08:12:48 -0400

    actualizando imagenes de erosol y europrotect

--- app/components/productos/components/ProductDetail.tsx
+++ app/components/productos/components/ProductDetail.tsx
@@ -15,7 +15,7 @@ interface IProducto {
     nombre: string;
     subtitulo?: string;
     imagen?: string;
-
+    imagen2?: string;
     composicion?: string;
     caracteristicas?: string[];
     recomendaciones?: string[];
@@ -85,48 +85,48 @@ export const ProductDetail: FC<ProductDetailProps> = ({ product }) => {
 
     // const data = product.documentacion?.length === 6 ? cardsData : cardsData2
 
-    if(product.nombre ==="MOLDES CENEFAS RODILLOS"){
-        return(
+    if (product.nombre === "MOLDES CENEFAS RODILLOS") {
+        return (
             <>
-                    <div className="flex max-md:flex-col gap-12">              
+                <div className="flex max-md:flex-col gap-12">
                     {product.documentacion && product.documentacion.length > 0 && (
-                <div >
+                        <div >
 
-                    <ul className="list-none mt-5 ml-5">
-                        <li>
-                            <strong>{t("productsSection.documentation")}</strong>
-                        </li>
-                        {product.documentacion.map((doc, i) => (
-                            <li key={i} className="flex items-center text-lg justify-between  gap-2">
-                                <span>{doc.nombre}</span>
-                                    <a
-                                        href={doc.enlace}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="flex gap-1 items-center"
-                                    >
-                                        {doc.accion || "Descargar"}
-                                        <svg
-                                            className={`w-5 h-5 transition-all rotate-90`}
-                                            fill="none"
-                                            stroke="currentColor"
-                                            viewBox="0 0 24 24"
+                            <ul className="list-none mt-5 ml-5">
+                                <li>
+                                    <strong>{t("productsSection.documentation")}</strong>
+                                </li>
+                                {product.documentacion.map((doc, i) => (
+                                    <li key={i} className="flex items-center text-lg justify-between  gap-2">
+                                        <span>{doc.nombre}</span>
+                                        <a
+                                            href={doc.enlace}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="flex gap-1 items-center"
                                         >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={0.5}
-                                                d="M9 5l7 7-7 7"
-                                            />
-                                        </svg>
-                                    </a>
+                                            {doc.accion || "Descargar"}
+                                            <svg
+                                                className={`w-5 h-5 transition-all rotate-90`}
+                                                fill="none"
+                                                stroke="currentColor"
+                                                viewBox="0 0 24 24"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={0.5}
+                                                    d="M9 5l7 7-7 7"
+                                                />
+                                            </svg>
+                                        </a>
 
-                            </li>
-                        ))}
-                    </ul>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
-            )}
-                     </div> 
             </>
         )
     }
@@ -213,12 +213,23 @@ export const ProductDetail: FC<ProductDetailProps> = ({ product }) => {
             {/* Documentation (if it exists) */}
             {product.documentacion && product.documentacion.length > 0 && (
                 <div className="my-4 md:w-1/4 max-md:w-3/4">
-                    <Image
-                        src={product.imagen || "/images/no-image.png"}
-                        alt={product.nombre}
-                        width={180}
-                        height={180}
-                    />
+                    <section className="flex">
+                        <Image
+                            src={product.imagen || "/images/no-image.png"}
+                            alt={product.nombre}
+                            width={180}
+                            height={180}
+                        />
+                        {
+                            product.imagen2 &&
+                            <Image
+                                src={product.imagen2 || "/images/no-image.png"}
+                                alt={product.nombre}
+                                width={180}
+                                height={180}
+                            />
+                        }
+                    </section>
 
                     <ul className="list-none mt-5 ml-5">
                         <li>
@@ -227,27 +238,27 @@ export const ProductDetail: FC<ProductDetailProps> = ({ product }) => {
                         {product.documentacion.map((doc, i) => (
                             <li key={i} className="flex items-center justify-between mb-1.5  gap-2">
                                 <span>{doc.nombre}</span>
-                                    <a
-                                        href={doc.enlace}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="flex gap-1 items-center"
+                                <a
+                                    href={doc.enlace}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="flex gap-1 items-center"
+                                >
+                                    {doc.accion || "Descargar"}
+                                    <svg
+                                        className={`w-5 h-5 transition-all rotate-90`}
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
                                     >
-                                        {doc.accion || "Descargar"}
-                                        <svg
-                                            className={`w-5 h-5 transition-all rotate-90`}
-                                            fill="none"
-                                            stroke="currentColor"
-                                            viewBox="0 0 24 24"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={0.5}
-                                                d="M9 5l7 7-7 7"
-                                            />
-                                        </svg>
-                                    </a>
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={0.5}
+                                            d="M9 5l7 7-7 7"
+                                        />
+                                    </svg>
+                                </a>
 
                             </li>
                         ))}
