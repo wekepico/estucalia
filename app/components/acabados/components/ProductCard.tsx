a20f9697722a74423c4ef3b1ff39619253a31217
ArtStyles <fejames07@gmail.com>
2025-06-01 17:55:52 -0400

    Enhance HeroSection with unique keys for mapped elements, update ProductCard styles for improved visuals, and add hover effects to download links in ProductDetail for better user experience.

--- app/components/acabados/components/ProductCard.tsx
+++ app/components/acabados/components/ProductCard.tsx
@@ -16,20 +16,20 @@ export const ProductCard: React.FC<ProductCardProps> = ({ name, icon,id }) => {
 
     return (
         <div 
-            className="bg-gray-100 group hover:bg-stone-100 transition-colors cursor-pointer"
+            className="bg-gray-100 group hover:bg-stone-300 transition-colors cursor-pointer"
             onClick={()=>{window.location.href = "/producto/"+id}}
         >
-            <div className=" min-w-[110px] py-4 flex flex-col items-center text-center">
+            <div className=" min-w-[140px]  py-4 flex flex-col items-center text-center">
                 <div className="mb-2">
                     <Image
                         src={icon}
                         alt="Logo"
                         width={180}
                         height={100}
-                        className="h-12 md:h-12 w-auto"
+                        className="h-14 md:h-14 w-auto"
                     />
                 </div>
-                <h3 className="text-[0.6rem] font-[600] mb-1 max-w-[min-content]">{name.toLocaleUpperCase()}</h3>
+                <h3 title={name} className="text-[0.6rem] font-[600] mb-1 max-w-[min-content] line-clamp-2">{name.toLocaleUpperCase()}</h3>
             </div>
         </div>
     )
